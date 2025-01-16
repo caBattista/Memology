04988bf6ed38825d699a67510526159439436a31
Christopher Battista <christopherbattista95@gmail.com>
2025-01-16 22:23:39 +0100

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -3,6 +3,7 @@ import base64
 import io
 import ollama
 
+
 def image_to_base64(image_path):
     with Image.open(image_path) as img:
         buffered = io.BytesIO()
@@ -11,11 +12,37 @@ def image_to_base64(image_path):
         img_base64 = base64.b64encode(img_bytes).decode('utf-8')
         return img_base64
 
+
+def doImage(promt, image_path):
+    base64_image = image_to_base64(image_path)
+    response = ollama.chat(
+        model="llama3.2-vision:latest",
+        messages=[{
+            "role": "user",
+            "content": promt,
+            "images": [base64_image]
+        }],
+    )
+    return response['message']['content'].strip()
+
+
+def doText(promt):
+    response = ollama.chat(
+        model="llama3.2:latest",
+        messages=[{
+            "role": "user",
+            "content": promt
+        }],
+    )
+    return response['message']['content'].strip()
+
+
 def writeText(text, path):
     f = open(path, "w", encoding="utf-8")
     f.write(text)
     f.close()
     return text
+
 
 def log(text):
     print(text)
@@ -23,25 +50,3 @@ def log(text):
     f.write(text)
     f.close()
     return text
-
-def doImage(promt, image_path):
-    base64_image = image_to_base64(image_path)
-    response = ollama.chat(
-        model="llama3.2-vision:latest",
-        messages=[{
-        "role": "user",
-        "content": promt,
-        "images": [base64_image]
-        }],
-    )
-    return response['message']['content'].strip()
-
-def doText(promt):
-    response = ollama.chat(
-        model="llama3.2:latest",
-        messages=[{
-        "role": "user",
-        "content": promt
-        }],
-    )
-    return response['message']['content'].strip()
